86ce11e18b91ee62b636faf02f4026cc336f467c
Cyra W <cwestmere@xpansiv.com>
2023-02-13 17:54:18 -0800

    added error handling

--- hostname-picker.py
+++ hostname-picker.py
@@ -3,10 +3,9 @@ import json
 import os
 import random
 
-
 def main():
     parser = argparse.ArgumentParser(description='Generate a random name from a given category')
-    parser.add_argument('-c', '--category', type=str, choices=['birds', 'artists'], required=True, help='The category to choose from')
+    parser.add_argument('-c', '--category', type=str, choices=['birds', 'artists',], required=True, help='The category to choose from')
     args = parser.parse_args()
 
     category = args.category
@@ -21,6 +20,20 @@ def main():
         print(selected_item_with_hyphens)
     else:
         print(f'Error: File {filename} does not exist')
+        print_usage()
+
+def print_usage():
+    print("""
+    Usage: hostname-generator.py [-h] -c {category-name}
+    ex:    hostname-generator.py -c birds
+
+    Generate a random name from a given category
+
+    optional arguments:
+      -h, --help            show this help message and exit
+      -c, --category        The category to choose from
+                            Category is chosen by the filename in ./data/{filename}.json
+    """)
 
 if __name__ == '__main__':
     main()
